31fb4c5b2e9ae46d9a7d4bfc859f8b8637e3c94d
Samuel Juan <67590532+Samueljuan@users.noreply.github.com>
2024-05-22 21:52:14 +0700

    Delete Blast WhatsApp Group directory

--- Blast WhatsApp Group/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# macOS
-chrome_path="user-data-dir="+os.path.expanduser('~')+"/Library/Application Support/Google/Chrome/Chr11"
-
-# windows
-# chrome_path="user-data-dir="+os.path.expanduser('~')+"\\AppData\\Local\\Google\\Chrome\\User Data\\WhatsApp"
